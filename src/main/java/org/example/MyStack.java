daa5e662e001e0648b8d977ec9c76fb6c3fb2c12
Jason <chensong823@gmail.com>
2024-03-06 20:04:30 -0500

    step4: create instance and initialize

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -12,23 +12,23 @@ public class MyStack<E> extends ArrayList<E> {
     }
 
 
-//    public void push(E e) {
-//        delegate.add(e);
-//    }
     public void push(E e) {
-        add(e);
+        delegate.add(e);
     }
-
-//    public E pop() {
-//        if (isEmpty()) throw new EmptyStackException();
-//        return delegate.remove(delegate.size() - 1);
+//    public void push(E e) {
+//        add(e);
 //    }
 
     public E pop() {
         if (isEmpty()) throw new EmptyStackException();
-        E e = get(size() - 1);
-        remove(size() -1 );
-        return e;
+        return delegate.remove(delegate.size() - 1);
     }
+
+//    public E pop() {
+//        if (isEmpty()) throw new EmptyStackException();
+//        E e = get(size() - 1);
+//        remove(size() -1 );
+//        return e;
+//    }
 
 }
